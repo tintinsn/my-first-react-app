9662676d486cf8a90c09599b94d59d9e6c8ba236
tintinsn <tin.sn98@gmail.com>
2023-10-07 23:34:15 +0700

    add exercise axios post methode

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { PostDTO } from './types/dto'
 import Post from './components/Post'
 import { FormEvent, useState, useEffect } from 'react'
 import axios from 'axios'
+import { CreatePostDTO } from './types/dto'
 
 function App() {
   const [posts, setPosts] = useState<PostDTO[] | null>(null)
   const [newTitle, setNewTitle] = useState<string>('')
   const [newBody, setNewBody] = useState<string>('')
   const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [disableSubmit, setDisableSubmit] = useState<boolean>(false)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -36,18 +38,45 @@ function App() {
     fetchData()
   }, [])
 
-  const handleSubmit = (e: FormEvent) => {
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault()
     if (!posts) return
-    const currentPost = [...posts]
-    currentPost.push({
-      id: Math.floor(Math.random() * 1000),
+    // const currentPost = [...posts]
+    // currentPost.push({
+    //   id: Math.floor(Math.random() * 1000),
+    //   userId: Math.floor(Math.random() * 1000),
+    //   title: newTitle,
+    //   body: newBody,
+    // })
+
+    const newPostBogdy: CreatePostDTO = {
       userId: Math.floor(Math.random() * 1000),
+      id: Math.floor(Math.random() * 1000),
       title: newTitle,
       body: newBody,
-    })
+    }
+    setDisableSubmit(true)
+    try {
+      await axios
+        .post('https://jsonplaceholder.typicode.com/posts', newPostBogdy, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+        .then((response) => {
+          const currentPost = [...posts]
+          currentPost.push(response.data)
+          setPosts(currentPost)
+        })
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setDisableSubmit(false)
+    }
 
-    setPosts(currentPost)
+    // setPosts(currentPost)
+    setNewTitle('')
+    setNewBody('')
   }
   if (isLoading) return <h1>LOADING...</h1>
   return (
@@ -60,7 +89,9 @@ function App() {
         <input type="text" value={newTitle} onChange={(e) => setNewTitle(e.target.value)} required />
         <label>Body</label>
         <input type="text" value={newBody} onChange={(e) => setNewBody(e.target.value)} required />
-        <button>submit</button>
+        <button type="submit" disabled={disableSubmit}>
+          submit
+        </button>
       </form>
 
       <div className="feed-container">
